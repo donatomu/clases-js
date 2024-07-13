7c02a8edb268d09b7ba50ad4fcd0f3a041b62515
Daniel07Navarro <daniel7navarro7@gmail.com>
2024-07-12 21:09:49 -0500

    Nuevos cambios

---
+++ NODE-CLASES/index2.js
@@ -0,0 +1,6 @@
+const modulo = require('./modulo');
+const prueba = require('../PRUEBA-SQL/index.js');
+
+modulo.saludar(modulo.nombre);
+
+prueba.mostrarDatos();
---
+++ NODE-CLASES/modulo.js
@@ -0,0 +1,10 @@
+function saludar(nombre){
+    console.log(`Hola ${nombre}`);
+}
+
+let nombre = 'Daniel';
+
+module.exports = {
+    saludar,
+    nombre
+};
